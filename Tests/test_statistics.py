8b33b29547c9862e3f1ecef316105c52682e7537
williamaherman <bherman2424@gmail.com>
2020-11-08 19:46:58 -0500

    task: fix test_statistics median/mode functions

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -11,10 +11,9 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         seed(5)
         self.testData = randint(0, 10, 20)
-        self.mode_value = statistics.mode(self.testData)
         self.statistics = Statistics()
-        self.test_data_mode = CsvReader('Tests/Data/modeData.csv').data
-        self.test_data_mode = CsvReader('Tests/Data/medianData.csv').data
+        self.modeData= CsvReader('Tests/Data/modeData.csv').data
+        self.medianData = CsvReader('Tests/Data/medianData.csv').data
 
 
     def test_instantiate_calculator(self):
@@ -24,20 +23,27 @@ class MyTestCase(unittest.TestCase):
         mean = self.statistics.mean(self.testData)
         self.assertEqual(mean, 4.25)
 
-    def test_mode_calculator(self):
-        mode = self.statistics.mode(self.testData)
-        self.assertEqual(mode, self.mode_value
+    def test_mode_statistics(self):
+        for row in self.modeData:
+            result = row["Mode"]
+            data = []
+            keys = row.keys()
+            for x in keys:
+                if x != "Mode":
+                    data.append(row[x])
+            self.assertEqual(self.statistics.mode(data), float(result))
+        print("Mode Tested Successfully!")
 
     def test_median_statistics(self):
-        for row in self.test_data_median:
+        for row in self.medianData:
             result = row["Median"]
-                data = []
-                keys = row.keys()
-                for k in keys:
-                    if k != "Median":
-                        data.append(row[k])
+            data = []
+            keys = row.keys()
+            for x in keys:
+                if x != "Median":
+                    data.append(row[x])
                 self.assertEqual(self.statistics.median(data), float(result))
-            print("Median Test Passed")
+            print("Median Tested Successfully!")
 
 
 if __name__ == '__main__':
