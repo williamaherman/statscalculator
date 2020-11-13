5dce499e34c4fe093fa54811c0c351d1155deaec
Brad Traversy <ajpDrumm@gmail.com>
2020-11-13 17:51:02 -0500

    Task: changed around margin of error to use csv file

--- PopulationSample/MarginOfError.py
+++ PopulationSample/MarginOfError.py
@@ -1,11 +1,25 @@
 from Statistics import Zscore, StandardDeviation
 from Statistics.StandardDeviation import standardDeviation
 
-class MarginOfError:
-    @staticmethod
-    def MarginOfError(seed, data):
-        if len(data)==0:
-            raise ValueError("Data list can not be empty")
-        zsc = Zscore.zscore(seed, data)
-        stddev = StandardDeviation.standardDeviation(data)
-        return Zscore * standardDeviation
+
+def marginofError(self, a, b, c, d, e):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
+        e = int(e)
+        data = [a, b, c, d, e]
+
+        n = len([data])
+        num2 = self.standardDeviation([a, b, c, d, e])
+        num3 = round(self.square_root(n), 6)
+        value1 = self.division(num3, num2)
+        z = int(1.96)
+        self.result = round(self.multiplication(value1, z), 6)
+
+    except ZeroDivisionError:
+        print("Error. Cannot Divide by 0")
+    except ValueError:
+        print("Error. Invalid data inputs")
+    return self.result
