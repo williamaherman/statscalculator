3fd9a09aa2454d8f6abefef063ad9584d297f7c5
Brad Traversy <ajpDrumm@gmail.com>
2020-11-15 18:19:57 -0500

    Feature: simple random sampling working!

--- RanNumGen/RanNumGenHelper.py
+++ RanNumGen/RanNumGenHelper.py
@@ -5,3 +5,20 @@ class ListPick:
     @staticmethod
     def listPick(data):
         return random.choice(data)
+
+    @staticmethod
+    def listPickSeed(seed, data):
+        random.seed(seed)
+        return ListPick.listPick(data)
+
+    @staticmethod
+    def listPickList(nums, data):
+        temp = []
+        while len(temp) < nums:
+            temp.append(ListPick.listPick(data))
+        return temp
+
+    @staticmethod
+    def listPickListSeed(seed, nums, data):
+        random.seed(seed)
+        return ListPick.listPickList(nums, data)
